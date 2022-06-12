9115d79511cf1de920c58aea11c4001e1d9d6c95
iwanharyatno <iwnharry61@gmail.com>
2022-06-12 09:04:07 +0700

    Create homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import Button from './components/Button';
+
 function App() {
   return (
-      <div className="p-8">
+      <div className="p-8 mt-4 text-center">
 	      <h1 className="text-3xl text-blue-700 mb-3">Dlema Quiz App</h1>
-	      <p className="text-gray-400">Built with Vite + React + TailwindCSS</p>
+	      <p className="text-gray-400 mb-4">A simple and static quizz App. Made by <a href="https://github.com/iwanharyatno" className="text-blue-400">Iwan Haryatno</a>.</p>
+          <Button text="Start" />
       </div>
-  )
+  );
 }
-
-export default App
+export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,5 @@
+function Button(props) {
+    return <button type="button" className="px-4 py-1 bg-blue-700 text-white shadow-md hover:bg-blue-900 active:shadow-lg">{props.text}</button>;
+}
+
+export default Button;
