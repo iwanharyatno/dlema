e3918a2fd0eb68dfb534abec576928173699ee8b
iwanharyatno <iwnharry61@gmail.com>
2022-06-12 07:47:37 +0700

    Setup React project with TailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+      <div className="p-8">
+	      <h1 className="text-3xl text-blue-700 mb-3">Dlema Quiz App</h1>
+	      <p className="text-gray-400">Built with Vite + React + TailwindCSS</p>
+      </div>
+  )
+}
+
+export default App
