167a15880ddce78c8871b5998365de8e373a9bc5
iwanharyatno <iwnharry61@gmail.com>
2022-06-12 14:26:35 +0700

    Add answer validation and finish screen

--- src/App.jsx
+++ src/App.jsx
@@ -5,32 +5,73 @@ import Button from './components/Button';
 import Questions from './components/questions/Questions';
 
 import problems from './resources/problems.json';
+import correctAnswers from './resources/correct-answers.json';
 
 class App extends React.Component {
     state = {
-        quizStarted: false
+        quizStarted: false,
+        quizFinished: true,
+        accuration: 100
     }
 
-    startQuiz = () => {
+    start = () => {
         this.setState({
-            quizStarted: true
+            quizStarted: true,
+            quizFinished: false,
+            accuration: 0
         });
     }
+
+    restart = () => {
+        this.setState({
+            quizStarted: false,
+            quizFinished: false,
+            accuration: 0
+        });
+    }
+
+    finish = (accuration) => {
+        this.setState({
+            quizStarted: false,
+            quizFinished: true,
+            accuration: accuration
+        });
+    }
+
     render = () => {
         if (this.state.quizStarted) {
             return (
                 <div>
-                    <Navbar text="Dlema"/>
-                    <Questions problems={problems}/>
+                    <Navbar text="Dlema" />
+                    <Questions problems={problems} correctAnswers={correctAnswers} onFinish={this.finish} />
+                </div>
+            );
+        }
+
+        if (this.state.quizFinished) {
+            let accurationColor = "";
+            if (this.state.accuration < 50) accurationColor = "text-red-700";
+            if (this.state.accuration >= 50 && this.state.accuration < 80) accurationColor = "text-yellow-700";
+            if (this.state.accuration >= 80 && this.state.accuration < 90) accurationColor = "text-blue-700";
+            if (this.state.accuration >= 90) accurationColor = "text-green-700";
+
+            return (
+                <div>
+                    <Navbar text="Dlema" />
+                    <div className="p-4 text-center md:max-w-[50%] mx-auto">
+                        <h1 className="mb-3 text-3xl">Your Accuration:</h1>
+                        <div className={accurationColor + ' font-bold p-4 text-5xl mb-5'}>{this.state.accuration}%</div>
+                        <Button text="Play Again" onClick={this.restart}/>
+                    </div>
                 </div>
             );
         }
 
         return (
-            <div className="p-8 mt-4 text-center">
+            <div className="p-8 mt-12 text-center">
                 <h1 className="text-3xl text-blue-700 mb-3">Dlema Quiz App</h1>
     	        <p className="text-gray-400 mb-4">A simple and static quizz App. Made by <a href="https://github.com/iwanharyatno" className="text-blue-400">Iwan Haryatno</a>.</p>
-                <Button text="Start" onClick={this.startQuiz} />
+                <Button text="Start" onClick={this.start} />
             </div>
         );
     }
--- src/components/questions/Questions.jsx
+++ src/components/questions/Questions.jsx
@@ -16,7 +16,10 @@ class Questions extends React.Component {
     }
 
     nextQuestion = () => {
-        if (this.state.questionIndex === (this.props.problems.length - 1)) return;
+        if (this.state.questionIndex === (this.props.problems.length - 1)) {
+            this.computeAccuration();
+            return;
+        }
         this.setState({
             questionIndex: this.state.questionIndex+1
         });
@@ -37,15 +40,31 @@ class Questions extends React.Component {
         console.log(this.state.answers);
     }
 
+    computeAccuration = () => {
+        let points = 0;
+        this.state.answers.map((answer, index) => {
+            if (this.props.correctAnswers[index] === answer) points++;
+        });
+
+        const accuration = points * 100 / this.props.correctAnswers.length;
+        this.props.onFinish(accuration);
+    }
+
     render() {
         const problem = this.props.problems[this.state.questionIndex];
+        let nextDoneButton;
+        if (this.state.questionIndex === (this.props.problems.length - 1)) {
+            nextDoneButton = <Button text="DONE" className="flex-1 py-3" color="bg-green-700 hover:bg-green-900" onClick={this.computeAccuration} />
+        } else {
+            nextDoneButton = <Button text="NEXT" className="flex-1 py-3" onClick={this.nextQuestion} />
+        }
 
         return (
             <div className="p-5 my-4 mx-auto md:max-w-[50%]">
                 <MultipleChoices problem={problem} onAnswer={this.saveAnswer} preAnswer={this.state.answers[this.state.questionIndex]} />
                 <div className="flex mt-4">
                     <Button text="PREV" className="flex-1 py-3" onClick={this.prevQuestion}/>
-                    <Button text="NEXT" className="flex-1 py-3" onClick={this.nextQuestion}/>
+                    {nextDoneButton}
                 </div>
             </div>
         );
