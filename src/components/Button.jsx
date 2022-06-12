5f719754bff38222ac45997ef43684dcbac2ca4c
iwanharyatno <iwnharry61@gmail.com>
2022-06-12 13:38:53 +0700

    Enable button color customization

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,5 @@
 function Button(props) {
-    return <button type={props.type ?? 'button'} onClick={props.onClick} className={'m-2 px-4 py-1 bg-blue-700 text-white shadow-md hover:bg-blue-900 active:shadow-lg ' + props.className}>{props.text}</button>;
+    return <button type={props.type ?? 'button'} onClick={props.onClick} className={'m-2 px-4 py-1 text-white shadow-md active:shadow-lg ' + props.className + ' ' + (props.color ?? 'bg-blue-700 hover:bg-blue-900')}>{props.text}</button>;
 }
 
 export default Button;
